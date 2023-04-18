5449c71226abc6b2ad1f387f58b45950beaea65c
GrassyAirplane <euanlimcs@gmail.com>
2023-04-18 10:30:48 +0800

    Base for Feed

---
+++ client/src/components/home/Main.tsx
@@ -0,0 +1,20 @@
+const Main = () => {
+    return (
+        <section className="main">
+            <div>
+                Div 1
+            </div>
+            <div>
+                Div 2
+            </div>
+            <div>
+                Div 3
+            </div>
+            <div>
+                Div 4
+            </div>
+        </section>
+    )
+}
+
+export default Main
